0ca793d05096638345626a70aa912fdf06a61be1
Marty Sauser <msauser2@gmail.com>
2016-06-02 04:31:41 -0700

    Redesign with form, add verification

---
+++ admin/doCommand.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<meta id="meta" name="viewport" content="width=device-width; initial-scale=1.0" />
+<style>
+body {width: 100%;}
+h1 {font-size: 18pt;}
+h2 {font-size: 16pt;}
+</style>
+</head>
+<body>
+<?php
+if ($_POST['cmd'] != 'dropboxStatus' && $_POST['verify'] != "ytram") {
+  $title = "Error";
+  $output = array("Verification failed.");
+} else {
+  switch ($_POST['cmd']) {
+    case 'startDropbox':
+      $title="Start Dropbox";
+      exec("/home/groovymarty/bin/startDropbox", $output, $retval);
+      break;
+    case 'stopDropbox':
+      $title="Stop Dropbox";
+      exec("/home/groovymarty/bin/stopDropbox", $output, $retval);
+      break;
+    case 'dropboxStatus':
+      $title="Dropbox Status";
+      exec("/home/groovymarty/bin/dropbox.py status", $output, $retval);
+      break;
+    default:
+      $title="Error";
+      $output=array("No such command: ".$_POST['cmd']);
+  }
+}
+echo "<h1>Welcome to <a href=\"/\">admin.groovymarty.com</a></h1>\n";
+echo "<h2>$title</h2>\n";
+foreach ($output as $line) {
+  echo htmlspecialchars($line), "<br>\n";
+}
+if ($retval) {
+  echo "Returned: $retval";
+}
+if ($_POST['cmd'] == 'dropboxStatus') { ?>
+<p>
+<form method="post">
+<input type="hidden" name="cmd" value="dropboxStatus">
+<input type="submit" value="Again">
+</form>
+<?php } ?>
+</body>
+</html>
--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,22 @@
 <html>
 <head>
+<meta id="meta" name="viewport" content="width=device-width; initial-scale=1.0" />
+<style>
+body {width: 100%;}
+h1 {font-size: 18pt;}
+h2 {font-size: 16pt;}
+</style>
 </head>
 <body>
-<h1>Welcome to admin.groovymarty.com</h1>
-<a href="startDropbox.php">Start Dropbox</a><br>
-<a href="stopDropbox.php">Stop Dropbox</a><br>
-<a href="dropboxStatus.php">Dropbox Status</a><br>
+<form action="doCommand.php" method="post">
+<h1>Welcome to <a href="/">admin.groovymarty.com</a></h1>
+<select name="cmd">
+<option value="startDropbox">Start Dropbox</option>
+<option value="stopDropbox">Stop Dropbox</option>
+<option value="dropboxStatus" selected>Dropbox Status</option>
+</select>
+<p>Verify: <input name="verify"/>
+<input type="submit" value="Submit"></p>
+</form>
 </body>
 </html>
